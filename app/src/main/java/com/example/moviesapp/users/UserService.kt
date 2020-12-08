f288e23e4c6f69f0c4b1428f272ec820006300ec
JessicaVillanueva <51139862+JessicaVillanueva@users.noreply.github.com>
2020-12-08 08:26:11 -0700

    -

--- app/src/main/java/com/example/moviesapp/users/UserService.kt
+++ app/src/main/java/com/example/moviesapp/users/UserService.kt
@@ -1,8 +1,5 @@
 package com.example.moviesapp.users
 
-import com.example.moviesapp.auth.AuthItem
-import com.example.moviesapp.comments.Comment
-import com.example.moviesapp.comments.CommentItem
 import retrofit2.Call
 import retrofit2.http.*
 
@@ -13,19 +10,9 @@ interface UserService {
     fun login(@Field("email") email: String,
            @Field("password") password: String): Call<UserItem>
 
-
-
     //ruta para crear un usuario
     @POST("users")
     fun saveUser(
         @Body user: User?
     ): Call<User?>
-
-//    @POST("comment")
-//    @FormUrlEncoded
-//    fun saveComment(
-//            @Field("comment") comment: String?,
-//            @Field("movie_id") movie_id: Int,
-//            @Field("user_id") user_id: Int
-//    ): Call<CommentItem?>?
 }
